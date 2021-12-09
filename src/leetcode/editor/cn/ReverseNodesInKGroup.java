7183bb0d703bbfeae5b5d10c4abf3c67cfce0c23
zhuwenjin <zhuwenjin1997@163.com>
2021-12-09 22:33:01 +0800

    重命名

--- src/leetcode/editor/cn/ReverseNodesInKGroup.java
+++ src/leetcode/editor/cn/ReverseNodesInKGroup.java
@@ -1,57 +1,57 @@
-//给你一个链表，每 k 个节点一组进行翻转，请你返回翻转后的链表。 
+//给你一个链表，每 k 个节点一组进行翻转，请你返回翻转后的链表。
 //
-// k 是一个正整数，它的值小于或等于链表的长度。 
+// k 是一个正整数，它的值小于或等于链表的长度。
 //
-// 如果节点总数不是 k 的整数倍，那么请将最后剩余的节点保持原有顺序。 
+// 如果节点总数不是 k 的整数倍，那么请将最后剩余的节点保持原有顺序。
 //
-// 进阶： 
+// 进阶：
 //
-// 
-// 你可以设计一个只使用常数额外空间的算法来解决此问题吗？ 
-// 你不能只是单纯的改变节点内部的值，而是需要实际进行节点交换。 
-// 
 //
-// 
+// 你可以设计一个只使用常数额外空间的算法来解决此问题吗？
+// 你不能只是单纯的改变节点内部的值，而是需要实际进行节点交换。
 //
-// 示例 1： 
 //
-// 
+//
+//
+// 示例 1：
+//
+//
 //输入：head = [1,2,3,4,5], k = 2
 //输出：[2,1,4,3,5]
-// 
 //
-// 示例 2： 
 //
-// 
+// 示例 2：
+//
+//
 //输入：head = [1,2,3,4,5], k = 3
 //输出：[3,2,1,4,5]
-// 
 //
-// 示例 3： 
 //
-// 
+// 示例 3：
+//
+//
 //输入：head = [1,2,3,4,5], k = 1
 //输出：[1,2,3,4,5]
-// 
 //
-// 示例 4： 
 //
-// 
+// 示例 4：
+//
+//
 //输入：head = [1], k = 1
 //输出：[1]
-// 
 //
-// 
-// 
 //
-// 提示： 
 //
-// 
-// 列表中节点的数量在范围 sz 内 
-// 1 <= sz <= 5000 
-// 0 <= Node.val <= 1000 
-// 1 <= k <= sz 
-// 
+//
+//
+// 提示：
+//
+//
+// 列表中节点的数量在范围 sz 内
+// 1 <= sz <= 5000
+// 0 <= Node.val <= 1000
+// 1 <= k <= sz
+//
 // Related Topics 递归 链表 👍 1391 👎 0
 
 package leetcode.editor.cn;
@@ -99,16 +99,14 @@ class ReverseNodesInKGroup{
                 if (end == null) {
                     break;
                 }
-
-                // 翻转这一段链表
                 ListNode left = pre.next;
                 ListNode right = end.next;
                 end.next = null;
-                pre.next = revers(left);
-
+                // 翻转这一段链表
+                pre.next = reverse(left);
                 // pre 设置到 end 的位置
-                pre.next = right;
-                end.next = right;
+                pre = right;
+                end = right;
             }
             return dummy.next;
         }
@@ -117,11 +115,12 @@ class ReverseNodesInKGroup{
         /*
             pre = null, cur = head, cur->pre, 往链表后面迭代
          */
-        private ListNode revers(ListNode head) {
+        private ListNode reverse(ListNode head) {
             ListNode pre = null;
             ListNode cur = head;
-            while (cur.next != null) {
-                ListNode tmp = cur.next;
+            ListNode tmp;
+            while (cur != null) {
+                tmp = cur.next;
                 cur.next = pre;
 
                 pre = cur;
