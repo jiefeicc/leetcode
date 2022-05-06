52bc5b50af269570d32c118d62a5117a4f4f563b
zhuwenjin <zhuwenjin@163.com>
2022-05-06 21:43:37 +0800

    31. 下一个排列

--- src/leetcode/editor/cn/NextPermutation.java
+++ src/leetcode/editor/cn/NextPermutation.java
@@ -52,13 +52,17 @@ import java.util.Arrays;
 class NextPermutation{
     public static void main(String[] args){
         Solution solution = new NextPermutation().new Solution();
-        solution.nextPermutation(new int[]{1,2,3,4,6,5});
+        solution.nextPermutation(new int[]{1,2,3,4,5,8,7,6});
     }
     //leetcode submit region begin(Prohibit modification and deletion)
     class Solution {
         /*
-        123465
-        654321
+        思路：
+            1.下一个数比当前数大，所以将后面一个大数与前面一个小数交换
+            2.变换成的下一个数的增幅尽可能小
+                1.在尽可能靠右的低位进行交换，从后往前遍历。
+                2.将后面尽可能小的大数与前面的小数进行交换，12345876是把6和5交换，不是把7或8和5交换。
+                3.后面的大数交换到前面后，将大数后面的数升序排序，这样后面就是最小的排列，交换到前面的大数让总体变大了，所以总体就是字典序中下一个更大的排列。
          */
         public void nextPermutation(int[] nums) {
             int len = nums.length;
@@ -68,15 +72,16 @@ class NextPermutation{
             for (int i = len-1; i>0; i--) {
                 //从后往前找，找到最右边的升序
                 if (nums[i] > nums[i-1]) {
-                    // 将最右边第一个比nums[i-1]大的数与nums[i-1]交换
-                    // 这个数肯定是右边最小的数，这个数最大就是nums[i]
+                    // nums[i-1] 到 nums[i]是最右边的升序，nums[i]后面都是降序。
+                    // 从右往左找到最右边最小的比 nums[i-1] 大的数，交换。
                     for (int j=len-1; j>=i; j--) {
                         if (nums[j] > nums[i-1]) {
                             swap(nums, i-1, j);
-                            // 需要跳出for循环
+                            // 需要跳出for循环，不然后面还会发生交换影响结果。
                             break;
                         }
                     }
+                    // 交换过后，还需要把交换到前面的数后面的数升序排序，这样才能保证后面最小。
                     Arrays.sort(nums, i, len);
                     return;
                 }
