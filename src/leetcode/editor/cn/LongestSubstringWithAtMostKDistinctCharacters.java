6d1f4f83d248344677a65d5b709c049dee211694
zhuwenjin <zhuwenjin1997@163.com>
2022-08-07 23:44:54 +0800

    调整笔记

---
+++ src/leetcode/editor/cn/LongestSubstringWithAtMostKDistinctCharacters.java
@@ -0,0 +1,28 @@
+package leetcode.editor.cn;
+
+import java.util.*;
+
+class LongestSubstringWithAtMostKDistinctCharacters{
+    public static void main(String[] args){
+        Solution solution = new LongestSubstringWithAtMostKDistinctCharacters().new Solution();
+    }
+    //至多包含 K 个不同字符的最长子串
+    /*
+    给定一个字符串 s ，找出 至多 包含 k 个不同字符的最长子串 T。
+
+    输入: s = "eceba", k = 2
+    输出: 3
+    解释: 则 T 为 "ece"，所以长度为 3。
+
+    输入: s = "aa", k = 1
+    输出: 2
+    解释: 则 T 为 "aa"，所以长度为 2。
+     */
+    //leetcode submit region begin(Prohibit modification and deletion)
+    class Solution {
+        public int lengthOfLongestSubstringKDistinct(String s, int k) {
+            return 0;
+        }
+    }
+    //leetcode submit region end(Prohibit modification and deletion)
+}
