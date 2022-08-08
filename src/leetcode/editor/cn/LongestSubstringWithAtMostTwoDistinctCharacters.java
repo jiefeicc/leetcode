43b4254d9153de5de31f1cab6e7b5b23ef87c0f0
zhuwenjin <zhuwenjin1997@163.com>
2022-08-09 00:45:36 +0800

    159. 至多包含两个不同字符的最长子串 & 340. 至多包含 K 个不同字符的最长子串

--- src/leetcode/editor/cn/LongestSubstringWithAtMostTwoDistinctCharacters.java
+++ src/leetcode/editor/cn/LongestSubstringWithAtMostTwoDistinctCharacters.java
@@ -2,40 +2,30 @@ package leetcode.editor.cn;
 
 import java.util.*;
 
-class LongestSubstringWithAtMostKDistinctCharacters{
+class LongestSubstringWithAtMostTwoDistinctCharacters{
     public static void main(String[] args){
-        Solution solution = new LongestSubstringWithAtMostKDistinctCharacters().new Solution();
+        Solution solution = new LongestSubstringWithAtMostTwoDistinctCharacters().new Solution();
+        System.out.println(solution.lengthOfLongestSubstringTwoDistinct("eceba"));
     }
-    //至多包含 K 个不同字符的最长子串
-    /*
-    给定一个字符串 s ，找出 至多 包含 k 个不同字符的最长子串 T。
-
-    输入: s = "eceba", k = 2
-    输出: 3
-    解释: 则 T 为 "ece"，所以长度为 3。
-
-    输入: s = "aa", k = 1
-    输出: 2
-    解释: 则 T 为 "aa"，所以长度为 2。
-     */
+    //至多包含两个不同字符的最长子串
     //leetcode submit region begin(Prohibit modification and deletion)
     class Solution {
         /*
         解题思路：
             滑动窗口解法
-            注意：至多包含k个不同字符 && 最长子串，需要注意的是光包含k个不同字符还不行，还需要是最长子串。
+            注意：至多包含两个不同字符 && 最长子串，需要注意的是光包含两个不同字符还不行，还需要是最长子串。
             滑动窗口遍历 s，每次 r++, 控制窗口内不要出现三个不同字符，维护 res = max(res, r - l + 1);
-            如何控制窗口内不超过k个不同字符：
+            如何控制窗口内不出现三个不同字符：
                 维护一个 map<Character, Integer>，每次遍历都往 map中存储（遍历到的字节，该字节出现的频次）。
-                当 map.size > k 时，说明窗口内出现三个不同字符，所以左边界右移，更新map，直到 map.size <= k。
+                当 map.size > 2 时，说明窗口内出现三个不同字符，所以左边界右移，更新map，直到 map.size <= 2。
          */
-        public int lengthOfLongestSubstringKDistinct(String s, int k) {
+        public int lengthOfLongestSubstringTwoDistinct(String s) {
             Map<Character, Integer> map = new HashMap<>();
             int res = 0;
             for (int l = 0, r = 0; r < s.length(); r++) {
                 Character c = s.charAt(r);
                 map.put(c, map.getOrDefault(c, 0) + 1);
-                if (map.size() > k) {
+                if (map.size() > 2) {
                     char leftChar = s.charAt(l);
                     int leftCharSize = map.get(leftChar);
                     l++;
@@ -52,4 +42,5 @@ class LongestSubstringWithAtMostKDistinctCharacters{
         }
     }
     //leetcode submit region end(Prohibit modification and deletion)
+
 }
